0c0d38b51c94acaa333fa0b0959012f08fcc8e61
Udai Relan <udairelan@gmail.com>
2024-03-18 17:16:18 -0400

    Update workouts.js

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Workout = require('../models/workoutModel.js')
 
 const router = express.Router()
 
@@ -13,9 +14,19 @@ router.get('/:id', (req, res) => {
 })
 
 // POST a new workout
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  const {title, load, reps} = req.body;
+
+  try {
+    const workout = await Workout.create({title, load, reps})
+    res.status(200).json(workout)
+  } catch (error) {
+    res.status(400).json({mssg: 'Unable to create workout'})
+  }
+
   res.json({mssg: 'POST a new workout'})
 })
+
 
 // DELETE a workout
 router.delete('/:id', (req, res) => {
