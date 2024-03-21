8d5db96eb00dfd3b4826b2392e58b33976547fcb
Udai Relan <udairelan@gmail.com>
2024-03-21 23:15:25 +0000

    Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,23 @@
+import { useEffect, useState} from 'react'
 
 const Home = () => {
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch(`http://localhost:4000/api/workouts`)
+      const json = await response.json()
+
+      if(response.ok) {
+        setWorkouts(json)
+      } else {
+        r
+      }
+    }
+
+
+
+    fetchWorkouts()
+  }, [])
 
     return (
       <div className="home">
